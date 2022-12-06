57271363d0e8a4d62c44af6017f88033c934febb
stens0n <astenson010@gmail.com>
2022-12-06 11:26:09 -0500

    navbar updates

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,11 @@ import  {Bars3Icon}  from '@heroicons/react/24/outline';
 
 const Navbar = () => {
   return (
-    <div>
+    <div className='w-screen h-[80px] z-10 bg-zinc-200 fixed drop-shadow-lg'>
+        <div className='px-2 flex justify-between items-center w-full h-full'>
+
+        </div>
+
         <Bars3Icon className='w-5' />
     </div>
   )
