07a17285d195550617c22381a9615d67ae371776
stens0n <astenson010@gmail.com>
2022-12-06 12:00:16 -0500

    updates to index.css layer base. addding styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,16 @@ const Navbar = () => {
   return (
     <div className='w-screen h-[80px] z-10 bg-zinc-200 fixed drop-shadow-lg'>
         <div className='px-2 flex justify-between items-center w-full h-full'>
-
+            <div className='flex items-center'>
+              <h1 className='text-3xl font-bold mr-4 sm:text-4xl'>Stenson</h1>
+              <ul className='hidden md:flex'> 
+              <li>Home</li>
+              <li>About</li>
+              <li>Support</li>
+              <li>Platforms</li>
+              <li>Pricing</li>
+              </ul>
+            </div>
         </div>
 
         <Bars3Icon className='w-5' />
