220545b9c5f87b56a981f4dbcc8c596ff105ffdb
stens0n <astenson010@gmail.com>
2022-12-05 22:25:43 -0500

    new concept

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import  {Bars3Icon}  from '@heroicons/react/24/outline';
+
+const Navbar = () => {
+  return (
+    <div>
+        <Bars3Icon className='w-5' />
+    </div>
+  )
+}
+
+
+export default Navbar
+
+
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ["./src/**/*.{html,js,jsx}"],
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
