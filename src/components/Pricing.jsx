89f1abf5b2452736b6ce628538482c754335e624
stens0n <astenson010@gmail.com>
2022-12-10 12:25:57 -0500

    final changes to pricing tailwind styling

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -16,19 +16,34 @@ const Pricing = () => {
 
             <div className="grid md:grid-cols-2">
 
-                <div className="bg-white text-slate-900 m-4 p-8 rounded-xl shadow-2xl">
-                    <span className="uppercase px-3 py-1 bg-indigo-200 text-indigo-900 rounded-2xl text-sm">Basic Website</span>
+                <div className="bg-white text-slate-900 m-4 p-8 rounded-xl shadow-2xl relative">
+                    <span className="uppercase px-3 py-1 bg-indigo-200 text-indigo-900 rounded-2xl text-sm">Premium Website</span>
                     <div>
-                        <p className="text-6xl font-bold py-4 flex">$299<span className='text-xl text-slate-500 flex flex-col justify-end'>/per project</span></p>
+                        <p className="text-6xl font-bold py-4 flex">$399<span className='text-xl text-slate-500 flex flex-col justify-end'>/per project</span></p>
                     </div>
                     <p className="text-2xl py-8 text-slate-500">Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+                    <div className="text-2xl">
+                        <p className="flex py-4"><CheckIcon className='w-7 mr-5 text-green-600' />Lorem, ipsum dolor.</p>
+                        <p className="flex py-4"><CheckIcon className='w-7 mr-5 text-green-600' />Lorem, ipsum dolor.</p>
+                        <p className="flex py-4"><CheckIcon className='w-7 mr-5 text-green-600' />Lorem, ipsum dolor.</p>
+                        <p className="flex py-4"><CheckIcon className='w-7 mr-5 text-green-600' />Lorem, ipsum dolor.</p>
+                        <p className="flex py-4"><CheckIcon className='w-7 mr-5 text-green-600' />Lorem, ipsum dolor.</p>
+                        <button className="w-full py-4 my-4">Get Started</button>
+                    </div>
+                </div>
+                <div className="bg-white text-slate-900 m-4 p-8 rounded-xl shadow-2xl relative">
+                    <span className="uppercase px-3 py-1 bg-indigo-200 text-indigo-900 rounded-2xl text-sm">Business Professional Website</span>
                     <div>
+                        <p className="text-6xl font-bold py-4 flex">$999<span className='text-xl text-slate-500 flex flex-col justify-end'>/per project</span></p>
+                    </div>
+                    <p className="text-2xl py-8 text-slate-500">Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+                    <div className="text-2xl">
                         <p className="flex py-4"><CheckIcon className='w-7 mr-5 text-green-600' />Lorem, ipsum dolor.</p>
                         <p className="flex py-4"><CheckIcon className='w-7 mr-5 text-green-600' />Lorem, ipsum dolor.</p>
                         <p className="flex py-4"><CheckIcon className='w-7 mr-5 text-green-600' />Lorem, ipsum dolor.</p>
                         <p className="flex py-4"><CheckIcon className='w-7 mr-5 text-green-600' />Lorem, ipsum dolor.</p>
                         <p className="flex py-4"><CheckIcon className='w-7 mr-5 text-green-600' />Lorem, ipsum dolor.</p>
-                        <button>Get Started</button>
+                        <button className="w-full py-4 my-4">Get Started</button>
                     </div>
                 </div>
             </div>
